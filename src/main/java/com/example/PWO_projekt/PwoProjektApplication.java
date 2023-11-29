f2a0e36f2abc5454bfed5224439babad2a01d0cd
vyhivskyi <rushoskey@gmail.com>
2023-11-29 13:24:43 +0100

    first commit

---
+++ src/main/java/com/example/PWO_projekt/PwoProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.PWO_projekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwoProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwoProjektApplication.class, args);
+	}
+
+}
